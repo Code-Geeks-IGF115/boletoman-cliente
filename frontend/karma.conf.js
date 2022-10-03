a8bad69693ea964e09a8659a2442cc90e7b2c85e
camperitox <ml17017@ues.edu.sv>
2022-10-02 22:51:05 -0600

    🔧 Se modificaron las dependencias para el frontend

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/paises-angular'),
+      dir: require('path').join(__dirname, './coverage/boletoman'),
       subdir: '.',
       reporters: [
         { type: 'html' },
