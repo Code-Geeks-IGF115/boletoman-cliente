476e50c6500cf475131b99d70686e2ef9fa69084
camperitox <ml17017@ues.edu.sv>
2022-10-04 02:44:16 -0600

    ✨ Se agregó el componente del login y creaEvento, se puede logear con cuenta gmail

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//importando componentes
+import { LoginComponent } from './components/login/login.component';
+import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:' ', redirectTo:'creaEvento',pathMatch: 'full'},
+  {path:'login', component: LoginComponent},
+  {path:'creaEvento', component: CreaEventoComponent},
+  {path:'**', redirectTo:'login',pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
