26e4656ef0d8ec269af2ee1c63cd79ff58382b50
camperitox <ml17017@ues.edu.sv>
2022-10-04 23:45:09 -0600

    ✨ Se agregó el compoenente para logearse fuera de auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +8,13 @@ import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './components/login/login.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+/* Angular Material */
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+/* Angular Flex Layout */
+
 // Import the module from the SDK
 import { AuthModule } from '@auth0/auth0-angular';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
@@ -25,7 +32,11 @@ import { CreaEventoComponent } from './components/crea-evento/crea-evento.compon
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
-    
+    FormsModule, 
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     // Import the module into the application, with configuration
     AuthModule.forRoot({
       domain: 'boletoman.us.auth0.com',
