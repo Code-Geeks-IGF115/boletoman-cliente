8ca01ab68b5de149d77242e02b349bdf8545d280
sr-Cebolla <111895615+sr-Cebolla@users.noreply.github.com>
2022-10-06 09:47:37 -0600

    edite el archivo crear-evento.components.html y agrege los module necesarios en el archivo app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,14 @@ import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './components/login/login.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 // Import the module from the SDK
 import { AuthModule } from '@auth0/auth0-angular';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
@@ -21,8 +29,16 @@ import { CreaEventoComponent } from './components/crea-evento/crea-evento.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatCardModule,
     BrowserAnimationsModule,
+    MatCheckboxModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatToolbarModule,
     MatMenuModule,
     MatIconModule,
     
