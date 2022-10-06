e404e6524581cb7c2ee2cb8075956f2cb7f37c1f
camperitox <ml17017@ues.edu.sv>
2022-10-06 14:29:52 -0600

    🔧 correcciones en la ruta

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 //importando componentes
 import { LoginComponent } from './components/login/login.component';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
-import { CreaCategoriaComponent } from './components/crea-categoria/crear-categoria.component';
-
+import { CrearCategoriaComponent } from './components/crear-categoria/crear-categoria.component';
 const routes: Routes = [
   {path:' ', redirectTo:'creaEvento',pathMatch: 'full'},
   {path:'login', component: LoginComponent},
   {path:'creaEvento', component: CreaEventoComponent},
-  {path:'crearCategoria', component: CreaCategoriaComponent},
+  {path:'crearCategoria', component: CrearCategoriaComponent},
   {path:'**', redirectTo:'login',pathMatch: 'full'},
 ];
 
