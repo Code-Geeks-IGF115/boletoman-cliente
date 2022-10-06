950da35dfd109f7476556de18568d4ececcd95e1
Zeldax15 <williamzelada908@gmail.com>
2022-10-05 22:26:41 -0600

    Diseño de la sala de eventos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,9 @@ import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './components/login/login.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 // Import the module from the SDK
 import { AuthModule } from '@auth0/auth0-angular';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
@@ -27,6 +30,9 @@ import { SalasDeEventoComponent } from './components/salas-de-evento/salas-de-ev
     MatButtonModule,
     MatMenuModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     
     // Import the module into the application, with configuration
     AuthModule.forRoot({
