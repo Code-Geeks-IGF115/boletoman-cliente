48092b2cd5c99b2089f1531f9ea3bb561e47c171
Salvic <74329652+Salvic@users.noreply.github.com>
2022-10-06 13:54:53 -0600

    Commit

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 //importando componentes
 import { LoginComponent } from './components/login/login.component';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
+import { CreaCategoriaComponent } from './components/crea-categoria/crear-categoria.component';
 
 const routes: Routes = [
   {path:' ', redirectTo:'creaEvento',pathMatch: 'full'},
   {path:'login', component: LoginComponent},
   {path:'creaEvento', component: CreaEventoComponent},
+  {path:'crearCategoria', component: CreaCategoriaComponent},
   {path:'**', redirectTo:'login',pathMatch: 'full'},
 ];
 
