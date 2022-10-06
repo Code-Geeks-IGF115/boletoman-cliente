2ab2c18409ad574a9937eeaefb32a350087c089d
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-06 15:56:51 +0000

    security package

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
