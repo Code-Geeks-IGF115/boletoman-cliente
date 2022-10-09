1a3c5aaf40a9e67e06d1b90894503b02f66c775f
Salvic <74329652+Salvic@users.noreply.github.com>
2022-10-08 19:04:40 -0600

    Commit crearCategoria

--- frontend/src/app/components/crear-categoria/crear-categoria.component.ts
+++ frontend/src/app/components/crear-categoria/crear-categoria.component.ts
@@ -14,37 +14,3 @@ export class CrearCategoriaComponent implements OnInit {
 
 }
 
-/** 
-
-export interface CategoriaT {
-  categoria: string;
-  codigo: string;
-  precio: string;
-  fila: number;
-  columna: number;
-  meses: number;
-  boletoman: number;
-
-}
-
-const ELEMENT_DATA: CategoriaT[] = [
-  { categoria: 'Categoria 1', codigo: 'CAT1', precio: '$25.00',   fila: 1, columna: 1, meses: 4, boletoman: 12},
-  { categoria: 'Categoria 2', codigo: 'CAT2', precio: '$15.00', fila: 2, columna: 1, meses: 0, boletoman: 24}
-  
-];
-
-
- @title Basic use of `<table mat-table>`
- 
-@Component({
-  selector: 'crear-categoria.component',
-  styleUrls: ['crear-categoria.component.html'],
-  templateUrl: 'crear-categoria.component.css',
-})
-export class TableCategoria {
-  displayedColumns: string[] = ['categoria', 'codigo', 'precio', 'fila', 'columna', 'meses', 'boletoman'];
-  dataSource = ELEMENT_DATA;
-}
-
-*/
-
