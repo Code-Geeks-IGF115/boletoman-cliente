90f4f0fb53195f474ab154479ec6f79dcc8002f3
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2022-11-21 22:09:56 +0000

    registro y login estilo, más cookies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //importando componentes
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 import { CreaEventoComponent } from './components/crea-evento/crea-evento.component';
 import { VerSalaDeEventoComponent } from './components/ver-sala-de-evento/ver-sala-de-evento.component';
 import { CrearCategoriaComponent } from './components/crear-categoria/crear-categoria.component';
@@ -21,6 +22,7 @@ import { DetalleCompraComponent } from './components/detalle-compra/detalle-comp
 const routes: Routes = [
 
   {path:'login', component: LoginComponent},
+  {path:'registro', component: RegistroComponent},
   // {path:'registro', component: RegistroComponent},
   {path: '',
     component: BaseComponent,
